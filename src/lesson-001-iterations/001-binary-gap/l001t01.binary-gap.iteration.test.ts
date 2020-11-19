9d5040821dc7c796058796b360b83348c659b741
Christian Kluge <christian.kluge@software-developer.org>
2020-11-19 18:30:47 +0100

    #18 lesson-001, added: description: l001t01.*.solution.js test: l001t01.*.test.ts

--- src/lesson-001-iterations/001-binary-gap/l001t01.binary-gap.iteration.test.ts
+++ src/lesson-001-iterations/001-binary-gap/l001t01.binary-gap.iteration.test.ts
@@ -1,35 +1,40 @@
-/**
- * An array where each element is the input for the solution function. Syntax:
- *
- * [ testset1, testset2, ..., testsetN]
- *
- * Where test set is:
- *
- * {
- *   A: [x1, x2]
- * }
- *
- * Example:
- *
- * [
- *   { // test input 1
- *     A: [1, 2]
- *   },
- *   { // test input 2
- *     A: [5, 1001]
- *   }
- * ]
- */
-const testData = [];
-
 import solution = require('./l001t01.binary-gap.iteration.solution');
 
 describe('iteration test', () => {
-  it('test solution', () => {
-    testData.forEach((testSet) => {
-      console.log('Input:', testSet);
-      const output = solution(testSet);
-      console.log('Result:', output);
-    });
+  it('test N = 9 ', () => {
+    // binary => 1001
+    const output = solution(9);
+    // expected gap = 2
+    expect(output).toEqual(2);
+  });
+  it('test N = 529 ', () => {
+    // binary => 1000010001
+    const output = solution(529);
+    // expected gap = 4
+    expect(output).toEqual(4);
+  });
+  it('test N = 20 ', () => {
+    // binary => 10100
+    const output = solution(20);
+    // expected gap = 1
+    expect(output).toEqual(1);
+  });
+  it('test N = 15 ', () => {
+    // binary => 1111
+    const output = solution(15);
+    // expected gap = 0
+    expect(output).toEqual(0);
+  });
+  it('test N = 32 ', () => {
+    // binary => 1000000
+    const output = solution(32);
+    // expected gap = 0
+    expect(output).toEqual(0);
+  });
+  it('test N = 1041 ', () => {
+    // binary => 10000010001
+    const output = solution(9);
+    // expected gap = 5
+    expect(output).toEqual(5);
   });
 });
