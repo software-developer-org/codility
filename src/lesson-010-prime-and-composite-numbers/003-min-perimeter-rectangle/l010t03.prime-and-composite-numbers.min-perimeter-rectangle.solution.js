ce2be88e70368a06c74b186ded4e552e9b4bf534
RouvenGonzalez <rouven.gonzalez@software-developer.org>
2020-11-20 09:58:03 +0100

    #18 added docs from codillity for lesson-010

--- src/lesson-010-prime-and-composite-numbers/003-min-perimeter-rectangle/l010t03.prime-and-composite-numbers.min-perimeter-rectangle.solution.js
+++ src/lesson-010-prime-and-composite-numbers/003-min-perimeter-rectangle/l010t03.prime-and-composite-numbers.min-perimeter-rectangle.solution.js
@@ -1,10 +1,36 @@
 /**
- * https://app.codility.com/programmers/lessons/10-prime_and_composite_numbers/min_perimeter_rectangle/start/
+ * https://app.codility.com/programmers/lessons/10-prime_and_composite_numbers/min_perimeter_rectangle/
+ *
+ * An integer N is given, representing the area of some rectangle.
+ *
+ * The area of a rectangle whose sides are of length A and B is A * B, and the perimeter is 2 * (A + B).
+ *
+ * The goal is to find the minimal perimeter of any rectangle whose area equals N.
+ * The sides of this rectangle should be only integers.
+ *
+ * For example, given integer N = 30, rectangles of area 30 are:
+ *
+ * (1, 30), with a perimeter of 62,
+ * (2, 15), with a perimeter of 34,
+ * (3, 10), with a perimeter of 26,
+ * (5, 6), with a perimeter of 22.
+ *
+ * Write a function:
+ *
+ * function solution(N);
+ *
+ * that, given an integer N, returns the minimal perimeter of any rectangle whose area is exactly equal to N.
+ *
+ * For example, given an integer N = 30, the function should return 22, as explained above.
+ *
+ * Write an efficient algorithm for the following assumptions:
+ *
+ * N is an integer within the range [1..1,000,000,000].
  */
 
 function solution(N) {
-  // write your code in JavaScript (Node.js 8.9.4)
+  // write your code in JavaScript
 }
 
-// this is not part of the solution copied to the codility editor
+// export for test usage
 module.exports = solution;
