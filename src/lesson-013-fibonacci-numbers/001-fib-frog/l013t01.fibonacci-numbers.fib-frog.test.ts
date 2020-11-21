e98e319af509dc6f64082e89aa8cd283b12d3107
Christian Kluge <christian.kluge@software-developer.org>
2020-11-21 11:55:35 +0100

    #18 lesson-013, added: description: l013t01.*, l013t02.*.solution.js corrected test: l013t01.*, l013t02.*.test.ts

--- src/lesson-013-fibonacci-numbers/001-fib-frog/l013t01.fibonacci-numbers.fib-frog.test.ts
+++ src/lesson-013-fibonacci-numbers/001-fib-frog/l013t01.fibonacci-numbers.fib-frog.test.ts
@@ -1,39 +1,8 @@
-/**
- * An array where each element is the input for the solution function. Syntax:
- *
- * [ testset1, testset2, ..., testsetN]
- *
- * Where test set is:
- *
- * {
- *   A: [x1, x2],
- * }
- *
- * Example:
- *
- * [
- *   { // test input 1
- *     A: [1, 2],
- *   },
- *   { // test input 2
- *     A: [5, 1001],
- *   }
- * ]
- */
-const testData = [];
-
 import solution = require('./l013t01.fibonacci-numbers.fib-frog.solution');
 
 describe('fibFrog test', () => {
-  it('test solution', () => {
-    testData.forEach((testSet) => {
-      console.log('Input:', testSet);
-      const output = solution(testSet);
-      console.log('Result:', output);
-    });
-  });
-  it('test if fibFrog is 3', () => {
-    const example = [0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0];
-    expect(solution(example)).toEqual(3);
+  it('test Non Empty Array, Both Possible Values Occure', () => {
+    const output = solution([0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0]);
+    expect(output).toEqual(3);
   });
 });
--- src/lesson-013-fibonacci-numbers/002-ladder/l013t02.fibonacci-numbers.ladder.test.ts
+++ src/lesson-013-fibonacci-numbers/002-ladder/l013t02.fibonacci-numbers.ladder.test.ts
@@ -1,45 +1,8 @@
-/**
- * An array where each element is the input for the solution function. Syntax:
- *
- * [ testset1, testset2, ..., testsetN]
- *
- * Where test set is:
- *
- * {
- *   A: [x1, x2],
- *   B: [x3, x4],
- * }
- *
- * Example:
- *
- * [
- *   { // test input 1
- *     A: [ [1, 2],
- *     B: [5, 8] ],
- *   },
- *   { // test input 2
- *     A: [ [5, 1001],
- *     B: [8, 25] ],
- *   }
- * ]
- */
-const testData = [];
-
 import solution = require('./l013t02.fibonacci-numbers.ladder.solution');
 
 describe('ladder test', () => {
-  it('test solution', () => {
-    testData.forEach((testSet) => {
-      console.log('Input:', testSet);
-      const output = solution(testSet[0], testSet[1]);
-      console.log('Result:', output);
-    });
-  });
-  it('test if ladder is  [5,1,8,0,1]', () => {
-    const example = [
-      [4, 4, 5, 5, 1],
-      [3, 2, 4, 3, 1],
-    ];
-    expect(solution(example[0], example[1])).toEqual([5, 1, 8, 0, 1]);
+  it('test Array A > 1, Array B > 1', () => {
+    const ouput = solution([4, 4, 5, 5, 1], [3, 2, 4, 3, 1]);
+    expect(ouput).toEqual([5, 1, 8, 0, 1]);
   });
 });
