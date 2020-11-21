86d952f6417953eea64b85e851d510946bd428b9
Christian Kluge <christian.kluge@software-developer.org>
2020-11-21 12:47:26 +0100

    #18 lesson-007, formatted test: l007t02.*.test.ts

--- src/lesson-007-stacks-and-queues/002-fish/l007t02.stacks-and-queues.fish.test.ts
+++ src/lesson-007-stacks-and-queues/002-fish/l007t02.stacks-and-queues.fish.test.ts
@@ -2,9 +2,8 @@ import solution = require('./l007t02.stacks-and-queues.fish.solution');
 
 describe('fish test', () => {
   it('test A: Only Small Integers > 0, B: 1 Fish in Other Direction', () => {
-    const a = [4, 3, 2, 1, 5];
-    const b = [0, 1, 0, 0, 0];
-    // except fish 0 and 4 everybody dies
-    expect(solution(a, b)).toEqual(2);
+    const output = solution([4, 3, 2, 1, 5], [0, 1, 0, 0, 0]);
+    // except fish 0 and 4, everybody dies
+    expect(output).toEqual(2);
   });
 });
