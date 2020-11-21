c3f95dda8b1e2752d3bc719a8c9c4defa05779e2
Christian Kluge <christian.kluge@software-developer.org>
2020-11-21 18:16:37 +0100

    #18 lesson-099, corrected: l099t01.*, l099t03.*, l099t04.*, l099t05.*.solution.js l099t01.*, l099t03.*, l099t04.*, l099t05.*.test.ts l099t05.*.tree-height.test.ts NOT finished!

--- src/lesson-099-future-training/005-tree-height/l099t05.future-training.tree-height.test.ts
+++ src/lesson-099-future-training/005-tree-height/l099t05.future-training.tree-height.test.ts
@@ -1,28 +1,3 @@
-/**
- * An array where each element is the input for the solution function. Syntax:
- *
- * [ testset1, testset2, ..., testsetN]
- *
- * Where test set is:
- *
- * {
- *   T: TODO
- * }
- *
- * Example:
- *
- * [
- *   { // test input 1
- *     T: 5, [3, [20, none, none], [21, none, none]], [10, [1, none, none], none]
- *   },
- *   { // test input 2
- *     A: [5, 1001],
- *     K: 8,
- *   }
- * ]
- */
-const testData = [];
-
 import solution = require('./l099t05.future-training.tree-height.solution');
 
 class Tree {
@@ -39,10 +14,11 @@ class Tree {
 
 describe('treeHeight test', () => {
   it('test solution', () => {
-    testData.forEach((testSet) => {
-      console.log('Input:', testSet);
-      const output = solution(testSet);
-      console.log('Result:', output);
+    const output = solution({
+      x: 0,
+      l: Tree,
+      r: Tree,
     });
+    expect(output).toEqual();
   });
 });
