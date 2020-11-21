b15e776ae432bb1d1146218c7ad2733a4172fe22
Christian Kluge <christian.kluge@software-developer.org>
2020-11-21 12:25:07 +0100

    #18 lesson-003, formatted test: l003t01.*.test.ts

--- src/lesson-003-time-complexity/001-frog-jump/l003t01.time-complexity.frog-jump.test.ts
+++ src/lesson-003-time-complexity/001-frog-jump/l003t01.time-complexity.frog-jump.test.ts
@@ -4,10 +4,8 @@ describe('frogJump test', () => {
   it('test X = 10 > 1, D = 30 < Y = 85', () => {
     // more than one jump (D) needed, completed distance exceeds target position
     // 85 < (10 + 30 + 30 + 30)
-    const x = 10;
-    const y = 85;
-    const d = 30;
+    const output = solution(10, 85, 30);
     // expected result = 3
-    expect(solution(x, y, d)).toEqual(3);
+    expect(output).toEqual(3);
   });
 });
