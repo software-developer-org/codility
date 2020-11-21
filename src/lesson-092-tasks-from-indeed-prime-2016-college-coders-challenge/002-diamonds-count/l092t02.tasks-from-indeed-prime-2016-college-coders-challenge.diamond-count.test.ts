cd1006fa024200425766b146f12d5a85d253b226
Christian Kluge <christian.kluge@software-developer.org>
2020-11-21 18:12:48 +0100

    #18 lesson-092, corrected: l092t02.*, l092t03.*, l092t04.*.solution.js l092t02.*, l092t03.*, l092t04.*.test.ts

--- src/lesson-092-tasks-from-indeed-prime-2016-college-coders-challenge/002-diamonds-count/l092t02.tasks-from-indeed-prime-2016-college-coders-challenge.diamond-count.test.ts
+++ src/lesson-092-tasks-from-indeed-prime-2016-college-coders-challenge/002-diamonds-count/l092t02.tasks-from-indeed-prime-2016-college-coders-challenge.diamond-count.test.ts
@@ -1,52 +1,13 @@
-/**
- * An array where each element is the input for the solution function. Syntax:
- *
- * [ testset1, testset2, ..., testsetN]
- *
- * Where test set is:
- *
- * {
- *   X: [x1, x2],
- *   V: [x3, x4],
- * }
- *
- * Example:
- *
- * [
- *   { // test input 1
- *     X: [ [1, 2],
- *     Y: [5, 6] ],
- *   },
- *   { // test input 2
- *     X: [5, 10],
- *     Y: [8, 12] ],
- *   }
- * ]
- */
-const testData = [];
-
 import solution = require('./l092t02.tasks-from-indeed-prime-2016-college-coders-challenge.diamond-count.solution');
 
 describe('diamondCount test', () => {
-  it('test solution', () => {
-    testData.forEach((testSet) => {
-      console.log('Input:', testSet);
-      const output = solution(testSet[0], testSet[1]);
-      console.log('Result:', output);
-    });
-  });
-
   it('testing if solution(X = [1, 1, 2, 2, 2, 3, 3], Y = [3, 4, 1, 3, 5, 3, 4]), returns 2', () => {
-    const X = [1, 1, 2, 2, 2, 3, 3];
-    const Y = [3, 4, 1, 3, 5, 3, 4];
-    const output = solution(X, Y);
+    const output = solution([1, 1, 2, 2, 2, 3, 3], [3, 4, 1, 3, 5, 3, 4]);
     expect(output).toEqual(2);
   });
 
   it('testing if solution(X = [1, 2, 3, 3, 2, 1], Y = [1, 1, 1, 2, 2, 2]), returns 0', () => {
-    const X = [1, 2, 3, 3, 2, 1];
-    const Y = [1, 1, 1, 2, 2, 2];
-    const output = solution(X, Y);
+    const output = solution([1, 2, 3, 3, 2, 1], [1, 1, 1, 2, 2, 2]);
     expect(output).toEqual(0);
   });
 });
