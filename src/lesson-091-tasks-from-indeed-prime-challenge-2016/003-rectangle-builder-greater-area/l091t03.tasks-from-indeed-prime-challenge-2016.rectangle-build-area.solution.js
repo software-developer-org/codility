1d3707b7be21a3e625c20ab78c9d5e6b46434d88
Christian Kluge <christian.kluge@software-developer.org>
2020-11-21 18:10:44 +0100

    #18 lesson-091, corrected: l091t01.*, l091t02.*, l091t03.*, l091t04.*.solution.js l091t01.*, l091t02.*, l091t03.*, l091t04.*.test.ts

--- src/lesson-091-tasks-from-indeed-prime-challenge-2016/003-rectangle-builder-greater-area/l091t03.tasks-from-indeed-prime-challenge-2016.rectangle-build-area.solution.js
+++ src/lesson-091-tasks-from-indeed-prime-challenge-2016/003-rectangle-builder-greater-area/l091t03.tasks-from-indeed-prime-challenge-2016.rectangle-build-area.solution.js
@@ -1,15 +1,22 @@
 /**
- * https://app.codility.com/programmers/lessons/91-tasks_from_indeed_prime_2016_challenge/rectangle_builder_greater_area/start/
+ * https://app.codility.com/programmers/lessons/91-tasks_from_indeed_prime_2016_challenge/rectangle_builder_greater_area/
  *
- * Halfling Woolly Proudhoof is an eminent sheep herder. He wants to build a pen (enclosure) for his new flock of sheep. The pen will be rectangular and built from exactly four pieces of fence (so, the pieces of fence forming the opposite sides of the pen must be of equal length). Woolly can choose these pieces out of N pieces of fence that are stored in his barn. To hold the entire flock, the area of the pen must be greater than or equal to a given threshold X.
+ * Halfling Woolly Proudhoof is an eminent sheep herder. He wants to build a pen (enclosure) for his new flock of sheep.
+ * The pen will be rectangular and built from exactly four pieces of fence (so, the pieces of fence forming the opposite
+ * sides of the pen must be of equal length). Woolly can choose these pieces out of N pieces of fence that are stored in
+ * his barn. To hold the entire flock, the area of the pen must be greater than or equal to a given threshold X.
  *
- * Woolly is interested in the number of different ways in which he can build a pen. Pens are considered different if the sets of lengths of their sides are different. For example, a pen of size 1×4 is different from a pen of size 2×2 (although both have an area of 4), but pens of sizes 1×2 and 2×1 are considered the same.
+ * Woolly is interested in the number of different ways in which he can build a pen. Pens are considered different if the
+ * sets of lengths of their sides are different. For example, a pen of size 1×4 is different from a pen of size 2×2
+ * (although both have an area of 4), but pens of sizes 1×2 and 2×1 are considered the same.
  *
  * Write a function:
  *
  * function solution(A, X);
  *
- * that, given an array A of N integers (containing the lengths of the available pieces of fence) and an integer X, returns the number of different ways of building a rectangular pen satisfying the above conditions. The function should return −1 if the result exceeds 1,000,000,000.
+ * that, given an array A of N integers (containing the lengths of the available pieces of fence) and an integer X, returns
+ * the number of different ways of building a rectangular pen satisfying the above conditions. The function should return −1
+ * if the result exceeds 1,000,000,000.
  *
  * For example, given X = 5 and the following array A:
  *
@@ -25,13 +32,15 @@
  *
  *
  *
- * the function should return 2. The figure above shows available pieces of fence (on the left) and possible to build pens (on the right). The pens are of sizes 1x5 and 2x5. Pens of sizes 1×1 and 1×2 can be built, but are too small in area. It is not possible to build pens of sizes 2×3 or 3×5, as there is only one piece of fence of length 3.
+ * the function should return 2. The figure above shows available pieces of fence (on the left) and possible to build pens
+ * (on the right). The pens are of sizes 1x5 and 2x5. Pens of sizes 1×1 and 1×2 can be built, but are too small in area.
+ * It is not possible to build pens of sizes 2×3 or 3×5, as there is only one piece of fence of length 3.
  *
  * Write an efficient algorithm for the following assumptions:
  *
- * N is an integer within the range [0..100,000];
- * X is an integer within the range [1..1,000,000,000];
- * each element of array A is an integer within the range [1..1,000,000,000].
+ *    N is an integer within the range [0..100,000];
+ *    X is an integer within the range [1..1,000,000,000];
+ *    each element of array A is an integer within the range [1..1,000,000,000].
  */
 
 function solution(A, X) {
