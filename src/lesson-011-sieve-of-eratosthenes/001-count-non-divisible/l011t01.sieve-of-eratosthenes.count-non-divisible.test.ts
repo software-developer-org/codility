a2f0006ccbf62845272cb6fe195e8b3f67d80c0a
Christian Kluge <christian.kluge@software-developer.org>
2020-11-21 11:50:31 +0100

    #18 lesson-011, added: description: l011t01.*, l011t02.*.solution.js corrected test: l011t01.*, l011t02.*.test.ts

--- src/lesson-011-sieve-of-eratosthenes/001-count-non-divisible/l011t01.sieve-of-eratosthenes.count-non-divisible.test.ts
+++ src/lesson-011-sieve-of-eratosthenes/001-count-non-divisible/l011t01.sieve-of-eratosthenes.count-non-divisible.test.ts
@@ -1,39 +1,8 @@
-/**
- * An array where each element is the input for the solution function. Syntax:
- *
- * [ testset1, testset2, ..., testsetN]
- *
- * Where test set is:
- *
- * {
- *   A: [x1, x2],
- * }
- *
- * Example:
- *
- * [
- *   { // test input 1
- *     A: [1, 2],
- *   },
- *   { // test input 2
- *     A: [5, 1001],
- *   }
- * ]
- */
-const testData = [];
-
 import solution = require('./l011t01.sieve-of-eratosthenes.count-non-divisible.solution');
 
 describe('countNonDivisible test', () => {
-  it('test solution', () => {
-    testData.forEach((testSet) => {
-      console.log('Input:', testSet);
-      const output = solution(testSet);
-      console.log('Result:', output);
-    });
-  });
-  it('test if countNonDivisible is [2,4,3,2,0]', () => {
-    const example = [3, 1, 2, 3, 6];
-    expect(solution(example)).toEqual([2, 4, 3, 2, 0]);
+  it('test Array > 1, Possible Divisors > 1, Only Small Values', () => {
+    const output = solution([3, 1, 2, 3, 6]);
+    expect(output).toEqual([2, 4, 3, 2, 0]);
   });
 });
--- src/lesson-011-sieve-of-eratosthenes/002-count-semiprimes/l011t02.sieve-of-eratosthenes.count-semiprimes.test.ts
+++ src/lesson-011-sieve-of-eratosthenes/002-count-semiprimes/l011t02.sieve-of-eratosthenes.count-semiprimes.test.ts
@@ -1,45 +1,8 @@
-/**
- * An array where each element is the input for the solution function. Syntax:
- *
- * [ testset1, testset2, ..., testsetN]
- *
- * Where test set is:
- *
- * {
- *   N: n,
- *   P: [x1, x2, x3],
- *   Q: [x4, x5, x6]
- * }
- *
- * Example:
- *
- * [
- *   { // test input 1
- *     N: 4,
- *     P: [5, 10, 15],
- *     Q: [1, 2, 3],
- *   },
- *   { // test input 2
- *     N: 5,
- *     P: [8, 16, 32],
- *     Q: [2, 4, 8],
- *   }
- * ]
- */
-const testData = [];
-
 import solution = require('./l011t02.sieve-of-eratosthenes.count-semiprimes.solution');
 
 describe('countSemiprimes test', () => {
-  it('test solution', () => {
-    testData.forEach((testSet) => {
-      console.log('Input:', testSet);
-      const output = solution(testSet[0], testSet[1], testSet[2]);
-      console.log('Result:', output);
-    });
-  });
-  it('test if countSemiprimes is [10,4,0]', () => {
-    const example = [26, [1, 4, 16], [26, 10, 20]];
-    expect(solution(example[0], example[1], example[2])).toEqual([10, 4, 0]);
+  it('test Arrays P > 1, Array Q > 1, N > 1', () => {
+    const ouput = solution([26, [1, 4, 16], [26, 10, 20]]);
+    expect(ouput).toEqual([10, 4, 0]);
   });
 });
