a635166af6e271b7f907d12e559bd5d039a25656
Christian Kluge <christian.kluge@software-developer.org>
2020-11-21 12:27:12 +0100

    #18 lesson-004, formatted test: l004t01.*, l004t02.*.test.ts

--- src/lesson-004-counting-elements/001-frog-river-one/l004t01.counting-elements.frog-river-one.test.ts
+++ src/lesson-004-counting-elements/001-frog-river-one/l004t01.counting-elements.frog-river-one.test.ts
@@ -2,9 +2,8 @@ import solution = require('./l004t01.counting-elements.frog-river-one.solution')
 
 describe('frogRiver test', () => {
   it('test Array > 1, Crossing Possible', () => {
-    const x = 5;
-    const a = [1, 3, 1, 4, 2, 3, 5, 4];
+    const output = solution(5, [1, 3, 1, 4, 2, 3, 5, 4]);
     // expected result = A[6] = 6
-    expect(solution(x, a)).toEqual(6);
+    expect(output).toEqual(6);
   });
 });
--- src/lesson-004-counting-elements/002-max-counters/l004t02.counting-elements.max-counters.test.ts
+++ src/lesson-004-counting-elements/002-max-counters/l004t02.counting-elements.max-counters.test.ts
@@ -2,9 +2,8 @@ import solution = require('./l004t02.counting-elements.max-counters.solution');
 
 describe('maxCounters test', () => {
   it('test Small Integers', () => {
-    const n = 5;
-    const a = [3, 4, 4, 6, 1, 4, 4];
+    const output = solution(5, [3, 4, 4, 6, 1, 4, 4]);
     // expected result = [3, 2, 2, 4, 2]
-    expect(solution(n, a)).toEqual([3, 2, 2, 4, 2]);
+    expect(output).toEqual([3, 2, 2, 4, 2]);
   });
 });
