51630c3535fb7156e854060f8fcf36b7c077e41b
Christian Kluge <christian.kluge@software-developer.org>
2020-11-21 18:07:20 +0100

    #18 lesson-090, corrected: l090t01.*, l090t02.*, l090t03.*.solution.js l090t01.*, l090t02.*, l090t03.*.test.ts

--- src/lesson-090-tasks-from-indeed-prime-challenge-2015/001-flood-depth/l090t01.tasks-from-indeed-prime-challenge-2015.flood-depth.test.ts
+++ src/lesson-090-tasks-from-indeed-prime-challenge-2015/001-flood-depth/l090t01.tasks-from-indeed-prime-challenge-2015.flood-depth.test.ts
@@ -1,47 +1,13 @@
-/**
- * An array where each element is the input for the solution function. Syntax:
- *
- * [ testset1, testset2, ..., testsetN]
- *
- * Where test set is:
- *
- * {
- *   A: [x1, x2],
- * }
- *
- * Example:
- *
- * [
- *   { // test input 1
- *     A: [1, 2],
- *   },
- *   { // test input 2
- *     A: [5, 1001],
- *   }
- * ]
- */
-const testData = [];
-
 import solution = require('./l090t01.tasks-from-indeed-prime-challenge-2015.flood-depth.solution');
 
 describe('floodDepth test', () => {
-  it('test solution', () => {
-    testData.forEach((testSet) => {
-      console.log('Input:', testSet);
-      const output = solution(testSet);
-      console.log('Result:', output);
-    });
-  });
-
-  it('testing if solution(A = [1, 3, 2, 1, 2, 1, 5, 3, 3, 4, 2]), return 2', () => {
-    const A = [1, 3, 2, 1, 2, 1, 5, 3, 3, 4, 2];
-    const output = solution(A);
+  it('test Small Array > 1, Only Small Values < 10, Land Can Hold Water ', () => {
+    const output = solution([1, 3, 2, 1, 2, 1, 5, 3, 3, 4, 2]);
     expect(output).toEqual(2);
   });
-
-  it('testing if solution(A = [5, 8]), return 0', () => {
-    const A = [5, 8];
-    const output = solution(A);
+  it('test Array Length = 2, Small Values, Land Can Not Hold Water', () => {
+    const output = solution([5, 8]);
+    // result should return 0;
     expect(output).toEqual(0);
   });
 });
--- src/lesson-090-tasks-from-indeed-prime-challenge-2015/002-longest-password/l090t02.tasks-from-indeed-prime-challenge-2015.longest-password.test.ts
+++ src/lesson-090-tasks-from-indeed-prime-challenge-2015/002-longest-password/l090t02.tasks-from-indeed-prime-challenge-2015.longest-password.test.ts
@@ -1,47 +1,8 @@
-/**
- * An array where each element is the input for the solution function. Syntax:
- *
- * [ testset1, testset2, ..., testsetN]
- *
- * Where test set is:
- *
- * {
- *   S: '   ',
- * }
- *
- * Example:
- *
- * [
- *   { // test input 1
- *     S: 'xyz 012 zt',
- *   },
- *   { // test input 2
- *     S: '?abc 234mt lpo',
- *   }
- * ]
- */
-const testData = [];
-
 import solution = require('./l090t02.tasks-from-indeed-prime-challenge-2015.longest-password.solution');
 
 describe('longestPassword test', () => {
-  it('test solution', () => {
-    testData.forEach((testSet) => {
-      console.log('Input:', testSet);
-      const output = solution(testSet);
-      console.log('Result:', output);
-    });
-  });
-
-  it("testing if solution(S = 'test 5 a0A pass007 ?xy1' return 7", () => {
-    const S = 'test 5 a0A pass007 ?xy1';
-    const output = solution(S);
+  it('test Multiple Valid And Multiple Invalid Words, Spaces > 0, Non-Alphanumerical Chars', () => {
+    const output = solution('test 5 a0A pass007 ?xy1');
     expect(output).toEqual(7);
-  });
-
-  it("testing if solution(S = 'test' return -1", () => {
-    const S = 'test';
-    const output = solution(S);
-    expect(output).toEqual(-1);
   });
 });
