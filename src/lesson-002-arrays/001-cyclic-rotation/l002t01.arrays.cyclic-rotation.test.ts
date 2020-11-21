6304f814829e1539cd85368473becc8a1a6db947
Christian Kluge <christian.kluge@software-developer.org>
2020-11-21 12:24:12 +0100

    #18 lesson-002, formatted test: l002t01.*.test.ts

--- src/lesson-002-arrays/001-cyclic-rotation/l002t01.arrays.cyclic-rotation.test.ts
+++ src/lesson-002-arrays/001-cyclic-rotation/l002t01.arrays.cyclic-rotation.test.ts
@@ -2,21 +2,18 @@ import solution = require('./l002t01.arrays.cyclic-rotation.solution');
 
 describe('rotation test', () => {
   it('test Array Length < Times of Rotation ', () => {
-    const a = [3, 8, 9, 7, 6];
-    const k = 3;
+    const output = solution([3, 8, 9, 7, 6], 3);
     // expected output [9, 7, 6, 3, 8]
-    expect(solution(a, k)).toEqual([9, 7, 6, 3, 8]);
+    expect(output).toEqual([9, 7, 6, 3, 8]);
   });
   it('test Array Length = Times of Rotation ', () => {
-    const a = [1, 2, 3, 4];
-    const k = 4;
+    const output = solution([1, 2, 3, 4], 4);
     // expected output [1, 2, 3, 4]
-    expect(solution(a, k)).toEqual([1, 2, 3, 4]);
+    expect(output).toEqual([1, 2, 3, 4]);
   });
   it('test Same Values & Array Length < Times of Rotation ', () => {
-    const a = [0, 0, 0];
-    const k = 1;
+    const output = solution([0, 0, 0], 1);
     // expected output [0, 0, 0]
-    expect(solution(a, k)).toEqual([0, 0, 0]);
+    expect(output).toEqual([0, 0, 0]);
   });
 });
