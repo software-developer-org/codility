e12e8e9681a8d4378aa0ae9b2918a1aa8fd31e08
Christian Kluge <christian.kluge@software-developer.org>
2020-11-21 18:04:18 +0100

    #18 lesson-016, corrected: l016t01.*, l016t02.*.solution.js l016t01.*, l016t02.test.ts

--- src/lesson-016-greedy-algorithms/001-max-non-overlapping-segments/l016t01.greedy-algorithms.max-non-overlapping-segments.test.ts
+++ src/lesson-016-greedy-algorithms/001-max-non-overlapping-segments/l016t01.greedy-algorithms.max-non-overlapping-segments.test.ts
@@ -1,47 +1,11 @@
-/**
- * An array where each element is the input for the solution function. Syntax:
- *
- * [ testset1, testset2, ..., testsetN]
- *
- * Where test set is:
- *
- * {
- *   A: [x1, x2],
- *   B: [x3, x4],
- * }
- *
- * Example:
- *
- * [
- *   { // test input 1
- *     A: [ [1, 2],
- *     B: [5, 8] ],
- *   },
- *   { // test input 2
- *     A: [ [5, 11],
- *     B: [9, 11] ],
- *   }
- * ]
- */
-const testData = [];
-
 import solution = require('./l016t01.greedy-algorithms.max-non-overlapping-segments.solution');
 
 describe('maxNonoverlappingSegments test', () => {
-  it('test solution', () => {
-    testData.forEach((testSet) => {
-      console.log('Input:', testSet);
-      const output = solution(testSet[0], testSet[1]);
-      console.log('Result:', output);
-    });
-  });
-
-  it('testing solution(A, B), should return 3', () => {
-    // non-overlapping set of segments from arrays A = [1, 3, 7, 9, 9] and B = [5, 6, 8, 9, 10]
-    const A = [1, 3, 7, 9, 9];
-    const B = [5, 6, 8, 9, 10];
-    const result = 3;
+  it('test Array A > 0, Array B > 0, All Values Small AND > 0', () => {
+    // non-overlapping set of segments from arrays
+    // A = [1, 3, 7, 9, 9] and B = [5, 6, 8, 9, 10]
+    const output = solution([1, 3, 7, 9, 9], [5, 6, 8, 9, 10]);
     // expected result should be 3
-    expect(solution(A, B)).toEqual(result);
+    expect(output).toEqual(3);
   });
 });
