c5bef341a1fc04f39c6854e5b642f9fdbdd37ef8
Christian Kluge <christian.kluge@software-developer.org>
2020-11-21 11:57:47 +0100

    #18 lesson-14, added: description: l014t01.*, l014t02.*.solution.js corrected test: l014t01.*, l014t02.*.test.ts

--- src/lesson-014-binary-search-algorithm/001-min-max-division/l014t01.binary-search-algorithm.min-max-division.test.ts
+++ src/lesson-014-binary-search-algorithm/001-min-max-division/l014t01.binary-search-algorithm.min-max-division.test.ts
@@ -1,45 +1,8 @@
-/**
- * An array where each element is the input for the solution function. Syntax:
- *
- * [ testset1, testset2, ..., testsetN]
- *
- * Where test set is:
- *
- * {
- *   K: k,
- *   M: m,
- *   A: [x1, x2],
- * }
- *
- * Example:
- *
- * [
- *   { // test input 1
- *     K: 1,
- *     M: 5,
- *     A: [2, 4],
- *   },
- *   { // test input 2
- *     K: 3,
- *     M: 5,
- *     A: [2, 4, 7, 8],
- *   }
- * ]
- */
-const testData = [];
-
 import solution = require('./l014t01.binary-search-algorithm.min-max-division.solution');
 
 describe('minMaxDivision test', () => {
-  it('test solution', () => {
-    testData.forEach((testSet) => {
-      console.log('Input:', testSet);
-      const output = solution(testSet[0], testSet[1], testSet[2]);
-      console.log('Result:', output);
-    });
-  });
-  it('test if minMaxDivision is 6', () => {
-    const example = [3, 5, [2, 1, 5, 1, 2, 2, 2]];
-    expect(solution(example[0], example[1], example[2])).toEqual(6);
+  it('test K > 1, M > 1, Values Array > 0, No Empty Blocks', () => {
+    const output = solution(3, 5, [2, 1, 5, 1, 2, 2, 2]);
+    expect(output).toEqual(6);
   });
 });
--- src/lesson-014-binary-search-algorithm/002-nailing-planks/l014t02.binary-search-algorithm.nailing-planks.test.ts
+++ src/lesson-014-binary-search-algorithm/002-nailing-planks/l014t02.binary-search-algorithm.nailing-planks.test.ts
@@ -1,49 +1,8 @@
-/**
- * An array where each element is the input for the solution function. Syntax:
- *
- * [ testset1, testset2, ..., testsetN]
- *
- * Where test set is:
- *
- * {
- *   A: [x1, x2],
- *   B: [x3, x4],
- *   C: [x5, x6],
- * }
- *
- * Example:
- *
- * [
- *   { // test input 1
- *     A: [1, 2],
- *     B: [5, 7],
- *     C: [9, 10, 12],
- *   },
- *   { // test input 2
- *     A: [5, 8],
- *     B: [8, 9, 10],
- *     C: [4, 6, 8, 10],
- *   }
- * ]
- */
-const testData = [];
-
 import solution = require('./l014t02.binary-search-algorithm.nailing-planks.solution');
 
 describe('nailingPlanks test', () => {
-  it('test solution', () => {
-    testData.forEach((testSet) => {
-      console.log('Input:', testSet);
-      const output = solution(testSet[0], testSet[1], testSet[2]);
-      console.log('Result:', output);
-    });
-  });
-  it('test if nailingPlanks is 4', () => {
-    const example = [
-      [1, 4, 5, 8],
-      [4, 5, 9, 10],
-      [4, 6, 7, 10, 2],
-    ];
-    expect(solution(example[0], example[1], example[2])).toEqual(4);
+  it('test Array A > 1, Array B > 1, Array C > 1', () => {
+    const ouput = solution([1, 4, 5, 8], [4, 5, 9, 10], [4, 6, 7, 10, 2]);
+    expect(ouput).toEqual(4);
   });
 });
