0d8a73d565834ae278c00f3f70236d0b8ee0f4f7
Christian Kluge <christian.kluge@software-developer.org>
2020-11-21 18:07:20 +0100

    #18 lesson-090, corrected: l090t01.*, l090t02.*, l090t03.*.solution.js l090t01.*, l090t02.*, l090t03.*.test.ts

--- src/lesson-090-tasks-from-indeed-prime-challenge-2015/001-flood-depth/l090t01.tasks-from-indeed-prime-challenge-2015.flood-depth.solution.js
+++ src/lesson-090-tasks-from-indeed-prime-challenge-2015/001-flood-depth/l090t01.tasks-from-indeed-prime-challenge-2015.flood-depth.solution.js
@@ -1,9 +1,16 @@
 /**
- * https://app.codility.com/programmers/lessons/90-tasks_from_indeed_prime_2015_challenge/flood_depth/start/
+ * https://app.codility.com/programmers/lessons/90-tasks_from_indeed_prime_2015_challenge/flood_depth/
  *
- * You are helping a geologist friend investigate an area with mountain lakes. A recent heavy rainfall has flooded these lakes and their water levels have reached the highest possible point. Your friend is interested to know the maximum depth in the deepest part of these lakes.
+ * You are helping a geologist friend investigate an area with mountain lakes. A recent heavy rainfall has flooded
+ * these lakes and their water levels have reached the highest possible point. Your friend is interested to know the
+ * maximum depth in the deepest part of these lakes.
  *
- * We simplify the problem in 2-D dimensions. The whole landscape can be divided into small blocks and described by an array A of length N. Each element of A is the altitude of the rock floor of a block (i.e. the height of this block when there is no water at all). After the rainfall, all the low-lying areas (i.e. blocks that have higher blocks on both sides) are holding as much water as possible. You would like to know the maximum depth of water after this entire area is flooded. You can assume that the altitude outside this area is zero and the outside area can accommodate infinite amount of water.
+ * We simplify the problem in 2-D dimensions. The whole landscape can be divided into small blocks and described by
+ * an array A of length N. Each element of A is the altitude of the rock floor of a block (i.e. the height of this block
+ * when there is no water at all). After the rainfall, all the low-lying areas (i.e. blocks that have higher blocks on
+ * both sides) are holding as much water as possible. You would like to know the maximum depth of water after this entire
+ * area is flooded. You can assume that the altitude outside this area is zero and the outside area can accommodate infinite
+ * amount of water.
  *
  * For example, consider array A such that:
  *
@@ -20,7 +27,9 @@
  *     A[10] = 2
  * The following picture illustrates the landscape after it has flooded:
  *
- * The gray area is the rock floor described by the array A above and the blue area with dashed lines represents the water filling the low-lying areas with maximum possible volume. Thus, blocks 3 and 5 have a water depth of 2 while blocks 2, 4, 7 and 8 have a water depth of 1. Therefore, the maximum water depth of this area is 2.
+ * The gray area is the rock floor described by the array A above and the blue area with dashed lines represents the water
+ * filling the low-lying areas with maximum possible volume. Thus, blocks 3 and 5 have a water depth of 2 while
+ * blocks 2, 4, 7 and 8 have a water depth of 1. Therefore, the maximum water depth of this area is 2.
  *
  * Write a function:
  *
@@ -39,8 +48,7 @@
  * Write an efficient algorithm for the following assumptions:
  *
  * N is an integer within the range [1..100,000];
- * each element of array A is an integer within the range [1..100,000,000].
- *
+ * each element of array A is an integer within the range [1..100,000,000]. *
  */
 
 function solution(A) {
--- src/lesson-090-tasks-from-indeed-prime-challenge-2015/003-slalom-skiing/l090t03.tasks-from-indeed-prime-challenge-2015.slalom-skiing.test.ts
+++ src/lesson-090-tasks-from-indeed-prime-challenge-2015/003-slalom-skiing/l090t03.tasks-from-indeed-prime-challenge-2015.slalom-skiing.test.ts
@@ -1,47 +1,12 @@
-/**
- * An array where each element is the input for the solution function. Syntax:
- *
- * [ testset1, testset2, ..., testsetN]
- *
- * Where test set is:
- *
- * {
- *   A: [x1, x2],
- * }
- *
- * Example:
- *
- * [
- *   { // test input 1
- *     A: [1, 2],
- *   },
- *   { // test input 2
- *     A: [5, 1001],
- *   }
- * ]
- */
-const testData = [];
-
 import solution = require('./l090t03.tasks-from-indeed-prime-challenge-2015.slalom-skiing.solution');
 
-describe('slalomSkiing test', () => {
-  it('test solution', () => {
-    testData.forEach((testSet) => {
-      console.log('Input:', testSet);
-      const output = solution(testSet);
-      console.log('Result:', output);
-    });
-  });
-
-  it('testing if solution(A = [15, 13, 5, 7, 4, 10, 12, 8, 2, 11, 6, 9, 3], return 8', () => {
-    const A = [15, 13, 5, 7, 4, 10, 12, 8, 2, 11, 6, 9, 3];
-    const output = solution(A);
+  it('test Small Values, Changing Times of Direction = 2', () => {
+    const output = solution([15, 13, 5, 7, 4, 10, 12, 8, 2, 11, 6, 9, 3]);
     expect(output).toEqual(8);
   });
 
-  it('testing if solution(A = [1, 5], return 2', () => {
-    const A = [1, 5];
-    const output = solution(A);
+  it('test Very Small Array, Changing Times of Direstion = 0', () => {
+    const output = solution([1, 5]);
     expect(output).toEqual(2);
   });
 });
