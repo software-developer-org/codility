744821b33fa27d568dd08fcb1bd306f481fbd624
Christian Kluge <christian.kluge@software-developer.org>
2020-11-21 11:52:16 +0100

    #18 lesson-012, added: description: l012t01.*, l012t02.*.solution.js corrected test: l012t01.*, l012t02.*.test.ts

--- src/lesson-012-euclidean-algorithm/001-chocolates-by-number/l012t01.euclidean-algorithm.chocolates-by-numbers.ts
+++ src/lesson-012-euclidean-algorithm/001-chocolates-by-number/l012t01.euclidean-algorithm.chocolates-by-numbers.ts
@@ -1,42 +1,8 @@
-/**
- * An array where each element is the input for the solution function. Syntax:
- *
- * [ testset1, testset2, ..., testsetN]
- *
- * Where test set is:
- *
- * {
- *   N: [x1],
- *   M: [x2],
- * }
- *
- * Example:
- *
- * [
- *   { // test input 1
- *     N: [ [1],
- *     M: [5] ],
- *   },
- *   { // test input 2
- *     N: [ [59],
- *     M: [89] ],
- *   }
- * ]
- */
-const testData = [];
-
 import solution = require('./l012t01.euclidean-algorithm.chocolates-by-numbers.solution');
 
 describe('chocolatesByNumbers test', () => {
-  it('test solution', () => {
-    testData.forEach((testSet) => {
-      console.log('Input:', testSet);
-      const output = solution(testSet[0], testSet[1]);
-      console.log('Result:', output);
-    });
-  });
-  it('test if chocolatesByNumbers is 5', () => {
-    const example = [10, 4];
-    expect(solution(example[0], example[1])).toEqual(5);
+  it('Integer N > 1, Integer M > 1, All Integers Small', () => {
+    const ouput = solution(10, 4);
+    expect(ouput).toEqual(5);
   });
 });
--- src/lesson-012-euclidean-algorithm/002-common-prime-divisor/l012t02.euclidean-algorithm.common-prime-divisors.test.ts
+++ src/lesson-012-euclidean-algorithm/002-common-prime-divisor/l012t02.euclidean-algorithm.common-prime-divisors.test.ts
@@ -1,45 +1,8 @@
-/**
- * An array where each element is the input for the solution function. Syntax:
- *
- * [ testset1, testset2, ..., testsetN]
- *
- * Where test set is:
- *
- * {
- *   A: [x1, x2],
- *   B: [x3, x4],
- * }
- *
- * Example:
- *
- * [
- *   { // test input 1
- *     A: [ [1, 2],
- *     B: [5, 10] ],
- *   },
- *   { // test input 2
- *     A: [ [5, 1001],
- *     B: [8, 20] ],
- *   }
- * ]
- */
-const testData = [];
-
 import solution = require('./l012t02.euclidean-algorithm.common-prime-divisors.solution');
 
 describe('commonPrimeDivisor test', () => {
-  it('test solution', () => {
-    testData.forEach((testSet) => {
-      console.log('Input:', testSet);
-      const output = solution(testSet[0], testSet[1]);
-      console.log('Result:', output);
-    });
-  });
-  it('test if commonPrimeDivisor is 1', () => {
-    const example = [
-      [15, 10, 3],
-      [75, 30, 5],
-    ];
-    expect(solution(example)).toEqual(1);
+  it('test Array A > 1, Array B > 1, Only One Possible Pairing', () => {
+    const output = solution([15, 10, 3], [75, 30, 5]);
+    expect(output).toEqual(1);
   });
 });
