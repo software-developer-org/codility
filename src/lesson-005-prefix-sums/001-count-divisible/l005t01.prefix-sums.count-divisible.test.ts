e0202563e241aa1585a0a0c38bdbf64468234c58
Christian Kluge <christian.kluge@software-developer.org>
2020-11-21 12:46:30 +0100

    #18 lesson-005, formatted test: l005t01.*, l005t02.*.test.ts renamed l005t01.*.genomic-range-query.test.ts -> l005t02.*.genomic-range-query.test.ts

--- src/lesson-005-prefix-sums/001-count-divisible/l005t01.prefix-sums.count-divisible.test.ts
+++ src/lesson-005-prefix-sums/001-count-divisible/l005t01.prefix-sums.count-divisible.test.ts
@@ -2,11 +2,9 @@ import solution = require('./l005t01.prefix-sums.count-divisible.solution');
 
 describe('countDivisible test', () => {
   it('test All Integers Samll and in Range > 0, Divisor > 1', () => {
-    const a = 6;
-    const b = 11;
-    const k = 2;
+    const output = solution(6, 11, 2);
     // 6, 8, 10 divisible by 2
     // result = 3
-    expect(solution(a, b, k)).toEqual(3);
+    expect(output).toEqual(3);
   });
 });
--- src/lesson-005-prefix-sums/002-genomic-range-query/l005t02.prefix-sums.genomic-range-query.test.ts
+++ src/lesson-005-prefix-sums/002-genomic-range-query/l005t02.prefix-sums.genomic-range-query.test.ts
@@ -2,10 +2,8 @@ import solution = require('./l005t02.prefix-sums.genomic-range-query.solution');
 
 describe('genRngQuery test', () => {
   it('test All Nucleotides Occure, ', () => {
-    const s = 'CAGCCTA ';
-    const p = [2, 5, 0];
-    const q = [4, 5, 6];
+    const output = solution('CAGCCTA ', [2, 5, 0], [4, 5, 6]);
     // expected result = [2, 4, 1]
-    expect(solution(s, p, q)).toEqual([2, 4, 1]);
+    expect(output).toEqual([2, 4, 1]);
   });
 });
